791dedbf8114c83f8dc9108bd59b4b2cd6011d51
Harrison Evans <harrisoncaldwellevans@gmail.com>
2021-08-04 20:17:07 -0700

    Create Hey, your up to .bat

---
+++ Hey, your up to .bat
@@ -0,0 +1,12 @@
+# Script:                       challenge 8
+# Author:                       Harrison Evans
+# date:                             8/4/21
+# Purpose:                      write a batch file.
+# the goal is to copie Jorge's project files to his new E drive.
+# in Task scheduler you create task and sett the timee jsut before the High moon, midnight.
+# Main 
+ROBOCOPY C:\users\j.thompson\desktop\MyProjectFiles E:\users\j.thompson\desktop
+# End
+
+
+# I fear i messed up my github remote push'ing? not sure how to say it. i plan to fix it all by the weekend. I had to do a good ole copy and paste. 
